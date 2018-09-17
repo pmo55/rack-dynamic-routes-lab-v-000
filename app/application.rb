5713f1b2aa606c449d27860e86788502302a81f8
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-09-17 19:49:58 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     req = Rack::Response.new(env)
     if req.path.match(/items/)
       item_name= req.path.split("/items/").last 
-      
+      if @@
       
     else
       resp.status = 404
